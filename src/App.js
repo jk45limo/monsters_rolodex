fba8cfb51d2c340e6a235eb2f0c3d8bd254877b5
Justus Kiplimo <59371869+jk45limo@users.noreply.github.com>
2022-08-15 22:55:09 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     };
   }
 
@@ -19,6 +20,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <input type='search' placeholder='search monsters' onChange={e => console.log(e)}/>
         <CardList monsters={this.state.monsters}/>
           
       </div>
--- src/components/card/card.components.jsx
+++ src/components/card/card.components.jsx
@@ -4,7 +4,7 @@ import './card.styles.css';
 
 export const Card = props => (
 <div className='card-container'>
-    <img 
+    <img
         alt='monster' 
         src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}/>
     <h1> {props.monster.name} </h1>
